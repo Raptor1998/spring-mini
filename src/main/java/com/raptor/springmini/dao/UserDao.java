b4eb17ce8603a1aa61d173c4e50ea97f0039f554
陈文豪 <chenwenhao.0401@bytedance.com>
2022-07-04 20:56:01 +0800

    master spring3.xml

--- src/main/java/com/raptor/springmini/dao/UserDao.java
+++ src/main/java/com/raptor/springmini/dao/UserDao.java
@@ -14,9 +14,9 @@ public class UserDao {
     private static Map<String, String> hashMap = new HashMap<>();
 
     static {
-        hashMap.put("10001", "小傅哥");
+        hashMap.put("10001", "raptor2");
         hashMap.put("10002", "八杯水");
-        hashMap.put("10003", "阿毛");
+        hashMap.put("10003", "spring3333");
     }
 
     public String queryUserName(String uId) {
